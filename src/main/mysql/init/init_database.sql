1027d50b1f9ec417a1be2ac6563c50c55f5bad3e
Chamith Siriwardena <chamith@gmail.com>
2022-03-24 09:59:34 +0530

    new files

---
+++ src/main/mysql/init/init_database.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE learnright;
+CREATE USER 'springuser'@'%' identified by 'learnright_spring';
+
+GRANT ALL on learnright.* to 'springuser'@'%';
+
+USE learnright;
+    
+DROP TABLE IF EXISTS `book_title`;
+CREATE TABLE `book_title` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` bigint(20) NOT NULL,
+  `book_title_id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `status` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
